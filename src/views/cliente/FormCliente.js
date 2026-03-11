0910ede407638c326237e600cabcd14e212bd809
Victor Santos <victorsantos2004xy@gmail.com>
2026-03-10 22:02:11 -0300

    aula dia 04

--- src/views/cliente/FormCliente.js
+++ src/views/cliente/FormCliente.js
@@ -1,139 +1,157 @@
-import axios from "axios";
-import InputMask from "comigo-tech-react-input-mask";
-import { useState } from "react";
-import { Button, Container, Divider, Form, Icon } from "semantic-ui-react";
-import MenuSistema from "../../MenuSistema";
+import InputMask from 'comigo-tech-react-input-mask';
+import { React, useState } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
 export default function FormCliente() {
-  const [nome, setNome] = useState();
-  const [cpf, setCpf] = useState();
-  const [dataNascimento, setDataNascimento] = useState();
-  const [foneCelular, setFoneCelular] = useState();
-  const [foneFixo, setFoneFixo] = useState();
 
-  function salvar() {
-    let clienteRequest = {
-      nome: nome,
-      cpf: cpf,
-      dataNascimento: dataNascimento,
-      foneCelular: foneCelular,
-      foneFixo: foneFixo,
-    };
+    const [nome, setNome] = useState();
+    const [cpf, setCpf] = useState();
+    const [dataNascimento, setDataNascimento] = useState();
+    const [foneCelular, setFoneCelular] = useState();
+    const [foneFixo, setFoneFixo] = useState();
 
-    axios
-      .post("http://localhost:8080/api/cliente", clienteRequest)
-      .then((response) => {
-        console.log("Cliente cadastrado com sucesso.");
-      })
-      .catch((error) => {
-        console.log("Erro ao incluir o um cliente.");
-      });
-  }
+    function salvar() {
 
-  return (
-    <>
-      <div>
-        <MenuSistema tela={"cliente"} />
+        let clienteRequest = {
+            nome: nome,
+            cpf: cpf,
+            dataNascimento: dataNascimento,
+            foneCelular: foneCelular,
+            foneFixo: foneFixo
+        }
 
-        <div style={{ marginTop: "3%" }}>
-          <Container textAlign="justified">...</Container>
-        </div>
-      </div>
-      <div>
-        <div style={{ marginTop: "3%" }}>
-          <Container textAlign="justified">
-            <h2>
-              {" "}
-              <span style={{ color: "darkgray" }}>
-                {" "}
-                Cliente &nbsp;
-                <Icon name="angle double right" size="small" />{" "}
-              </span>{" "}
-              Cadastro{" "}
-            </h2>
+        axios.post("http://localhost:8080/api/cliente", clienteRequest)
+            .then((response) => {
+                console.log('Cliente cadastrado com sucesso.')
+            })
+            .catch((error) => {
+                console.log('Erro ao incluir o um cliente.')
+            })
+    }
 
-            <Divider />
+    return (
 
-            <div style={{ marginTop: "4%" }}>
-              <Form>
-                <Form.Group widths="equal">
-                  <Form.Input
-                    required
-                    fluid
-                    label="Nome"
-                    maxLength="100"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                  />
+        <div>
 
-                  <Form.Input required fluid label="CPF">
-                    <InputMask
-                      required
-                      mask="999.999.999-99"
-                      value={cpf}
-                      onChange={(e) => setCpf(e.target.value)}
-                    />
-                  </Form.Input>
-                </Form.Group>
 
-                <Form.Group>
-                  <Form.Input fluid label="Fone Celular" width={6}>
-                    <InputMask
-                      mask="(99) 9999.9999"
-                      value={foneCelular}
-                      onChange={(e) => setFoneCelular(e.target.value)}
-                    />
-                  </Form.Input>
+            <div style={{ marginTop: '3%' }}>
 
-                  <Form.Input fluid label="Fone Fixo" width={6}>
-                    <InputMask
-                      mask="(99) 9999.9999"
-                      value={foneFixo}
-                      onChange={(e) => setFoneFixo(e.target.value)}
-                    />
-                  </Form.Input>
+                <Container textAlign='justified' >
 
-                  <Form.Input fluid label="Data Nascimento" width={6}>
-                    <InputMask
-                      mask="99/99/9999"
-                      maskChar={null}
-                      placeholder="Ex: 20/03/1985"
-                      value={dataNascimento}
-                      onChange={(e) => setDataNascimento(e.target.value)}
-                    />
-                  </Form.Input>
-                </Form.Group>
-              </Form>
+                    <h2> <span style={{ color: 'darkgray' }}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
 
-              <div style={{ marginTop: "4%" }}>
-                <Button
-                  type="button"
-                  inverted
-                  circular
-                  icon
-                  labelPosition="left"
-                  color="orange"
-                >
-                  <Icon name="reply" />
-                  Voltar
-                </Button>
+                    <Divider />
 
-                <Button
-                  inverted
-                  circular
-                  icon
-                  labelPosition="left"
-                  color="blue"
-                  floated="right"
-                  onClick={() => salvar()}
-                >
-                  <Icon name="save" />
-                  Salvar
-                </Button>
-              </div>
+                    <div style={{ marginTop: '4%' }}>
+
+                        <Form>
+
+                            <Form.Group widths='equal'>
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='Nome'
+                                    maxLength="100"
+                                    value={nome}
+                                    onChange={e => setNome(e.target.value)}
+                                />
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='CPF'>
+                                    <InputMask
+                                        required
+                                        mask="999.999.999-99"
+                                        value={cpf}
+                                        onChange={e => setCpf(e.target.value)}
+                                    />
+                                </Form.Input>
+
+                            </Form.Group>
+
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    label='Fone Celular'
+                                    width={6}>
+                                    <InputMask
+                                        mask="(99) 9999.9999"
+                                        value={foneCelular}
+                                        onChange={e => setFoneCelular(e.target.value)}
+                                    />
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Fone Fixo'
+                                    width={6}>
+                                    <InputMask
+                                        mask="(99) 9999.9999"
+                                        value={foneFixo}
+                                        onChange={e => setFoneFixo(e.target.value)}
+                                    />
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Data Nascimento'
+                                    width={6}
+                                >
+                                    <InputMask
+                                        mask="99/99/9999"
+                                        maskChar={null}
+                                        placeholder="Ex: 20/03/1985"
+                                        value={dataNascimento}
+                                        onChange={e => setDataNascimento(e.target.value)}
+                                    />
+                                </Form.Input>
+
+                            </Form.Group>
+
+                        </Form>
+
+                        <div style={{ marginTop: '4%' }}>
+
+                            <Link to={'/list-cliente'}>
+                                <Button
+                                    type="button"
+                                    inverted
+                                    circular
+                                    icon
+                                    labelPosition='left'
+                                    color='orange'
+                                >
+                                    <Icon name='reply' />
+                                    Voltar
+                                </Button>
+                            </Link>
+
+                            <Button
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                                onClick={() => salvar()}
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
+
+                        </div>
+
+                    </div>
+
+                </Container>
             </div>
-          </Container>
         </div>
-      </div>
-    </>
-  );
+
+    );
+
 }
