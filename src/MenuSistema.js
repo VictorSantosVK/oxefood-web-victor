33c0550ffe84b74258822ae1f40285e7cf69661c
aluno <aluno@email>
2026-04-29 20:07:06 -0300

    aula dia 29/04/2026

--- src/MenuSistema.js
+++ src/MenuSistema.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { logout } from './views/util/AuthenticationService';
 
 export default function MenuSistema(props) {
   return (
     <Menu inverted>
+      <Menu.Item
+                    className='navbar__item--mobile'
+                    onClick={logout}
+                    content='Sair'
+                    as={Link}
+                    to='/'
+                />
 
       <Menu.Item
         content="Cliente"
--- src/Rotas.js
+++ src/Rotas.js
@@ -1,38 +1,47 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 
-import Home from "./views/home/Home";
-
-import ListCliente from "./views/cliente/ListCliente";
-import FormCliente from "./views/cliente/FormCliente";
-
-import ListProduto from "./views/produto/ListProduto";
-import FormProduto from "./views/produto/FormProduto";
-
-import ListEntregador from "./views/entregador/ListEntregador";
-import FormEntregador from "./views/entregador/FormEntregador";
-
-import ListPromocao from "./views/promocao/ListPromocao";
-import FormPromocao from "./views/promocao/FormPromocao";
+import FormCliente from './views/cliente/FormCliente';
+import ListCliente from './views/cliente/ListCliente';
+import FormEntregador from './views/entregador/FormEntregador';
+import ListEntregador from './views/entregador/ListEntregador';
+import Home from './views/home/Home';
+import FormLogin from './views/login/FormLogin';
+import FormProduto from './views/produto/FormProduto';
+import ListProduto from './views/produto/ListProduto';
 
 function Rotas() {
-  return (
-    <Routes>
-      <Route path="/" element={<ListCliente />} />
+   return (
+       <>
+           <Routes>
+              
+               <Route path="/" element={ <FormLogin/> } />
 
-      <Route path="/list-cliente" element={<ListCliente />} />
-      <Route path="/form-cliente" element={<FormCliente />} />
-
-      <Route path="/list-produto" element={<ListProduto />} />
-      <Route path="/form-produto" element={<FormProduto />} />
-
-      <Route path="/list-entregador" element={<ListEntregador />} />
-      <Route path="/form-entregador" element={<FormEntregador />} />
-
-      <Route path="/list-promocao" element={<ListPromocao />} />
-      <Route path="/form-promocao" element={<FormPromocao />} />
-    </Routes>
-  );
+               <Route
+                   path="/home"
+                   element={
+                   <ProtectedRoute>
+                       <Home />
+                   </ProtectedRoute>
+                   }
+               />
+                  
+               <Route
+                   path="/list-cliente"
+                   element={
+                       <ProtectedRoute>
+                           <ListCliente />
+                       </ProtectedRoute>
+                   }
+               />
+       <Route path="list-cliente" element={ <ListCliente/> } />
+                <Route path="list-produto" element={ <ListProduto/> } />
+                <Route path="form-produto" element={ <FormProduto/> } />
+                <Route path="list-entregador" element={ <ListEntregador/> } />
+                <Route path="form-entregador" element={ <FormEntregador/> } />
+            </Routes>
+        </>
+    )
 }
 
-export default Rotas;
+export default Rotas
